98c18d181498e86abfc0242465280a2f885be2c6
Исаева Мария <isaeva-mariya@yandex.ru>
2020-02-17 18:21:35 +0300

    landing

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const less = require("gulp-less");
+const imagemin = require("gulp-imagemin");
+const pngquant = require('imagemin-pngquant');
+const autoprefixer = require("gulp-autoprefixer");
+const concat = require('gulp-concat-css');
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const server = require('browser-sync').create();
+
+gulp.task("css", function() {
+  return gulp.src("less/**/*.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest("css"))
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(csso())
+    .pipe(concat("style.css"))
+    .pipe(rename({
+        suffix: "-min",
+        extname: ".css"
+    }))
+    .pipe(gulp.dest("./css"))
+    .pipe(server.stream());
+});
+
+gulp.task('serve', function() {
+    server.init({
+        server: {
+            baseDir: "./",
+            }
+        });
+
+  gulp.watch("less/**/*.less", gulp.series("css"));
+	gulp.watch("**/*.html").on("change", server.reload);
+});
